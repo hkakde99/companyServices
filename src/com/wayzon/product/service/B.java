56003479b61dd4d799f10086196722772b26b7be
himanshu <himanshuv008@gmail.com>
2021-04-01 21:21:41 +0530

    first commit

---
+++ src/com/wayzon/product/service/B.java
@@ -0,0 +1,8 @@
+package com.wayzon.product.service;
+
+public class B {
+	public static void main(String [] args) {
+		System.out.println("this file contain specific Services");
+	}
+
+}
---
+++ src/com/wayzon/service/A.java
@@ -0,0 +1,8 @@
+package com.wayzon.service;
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("This file content common Services");
+	}
+
+}
